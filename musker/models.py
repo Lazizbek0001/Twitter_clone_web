df8d5a2bc814b31189b9a33b7ad1dbaffa581b71
Lazizbek Normatov <147187132+Lazizbek0001@users.noreply.github.com>
2024-07-04 15:42:40 +0500

    Update models.py

--- musker/models.py
+++ musker/models.py
@@ -37,12 +37,12 @@ class Profile(models.Model):
         blank=True)
     
     date_modified = models.DateTimeField(User, auto_now=True)
-    profile_image = models.ImageField(null=True, blank=True, upload_to="images/")
-    profile_bio = models.CharField(null=True, blank=True, max_length=500)
-    homepage_link = models.CharField(null=True, blank=True, max_length=100)
-    facebook_link = models.CharField(null=True, blank=True, max_length=100)
-    instagram_link = models.CharField(null=True, blank=True, max_length=100)
-    linkedln_link = models.CharField(null=True, blank=True, max_length=100)
+    profile_image = models.ImageField(blank=True, null=True, upload_to="images/")
+    profile_bio = models.CharField(blank=True, null=True, max_length=500)
+    homepage_link = models.CharField(blank=True, null=True, max_length=100)
+    facebook_link = models.CharField(blank=True, null=True, max_length=100)
+    instagram_link = models.CharField(blank=True, null=True, max_length=100)
+    linkedln_link = models.CharField(blank=True, null=True, max_length=100)
     
     def __str__(self):
         return self.user.username
